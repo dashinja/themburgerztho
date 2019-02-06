473bf26aa328c9dcaf5f74d40d4688763433dd13
DaShinja <byron@dashinja.com>
2019-02-05 21:26:51 -0500

    working on PUTting things in place

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,17 +1,32 @@
-const express = require('express')
-const router = express.Router()
-const burger = require('../model/burger')
+const express = require('express');
+const router = express.Router();
+const burger = require('../model/burger');
 
 //routes eh?
 
 router.get('/', (req, res) => {
   burger.all(result => {
     const visibleObject = {
-      burgers: result
-    }
-    console.log("Hi, I'm visible object: ", visibleObject)
-    res.render('index', visibleObject)
-  })
-})
+      burger: result
+    };
+    console.log("Hi, I'm visible object: ", visibleObject);
+    res.render('index', visibleObject);
+  });
+});
 
-module.exports = router
+router.post('/api/burger', (req, res) => {
+  burger.insert(
+    ['burger_name', 'devoured'],
+    [req.body.burgerName, req.body.devouredState],
+    (err, result) => {
+      if (err) throw err;
+
+      res.json({ id: result.insertId });
+    }
+  );
+
+  res.redirect('/');
+});
+
+router.put('/api/burger/:id', (req, res));
+module.exports = router;
