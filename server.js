82bdb310d2226cf9f5d3e96b85ceaf8bc7fd5803
DaShinja <byron@dashinja.com>
2019-02-06 16:17:54 -0500

    further heroku setup

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const PORT = 3000
+const express = require('express');
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const exphbs = require('express-handlebars')
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-let routes = require('./controllers/burgers_controller')
-app.use(routes)
+let routes = require('./controllers/burgers_controller');
+app.use(routes);
 
 app.listen(PORT, err => {
-  if (err) throw err
-  console.log(`Listening on port ${PORT}`)
-})
+  if (err) throw err;
+  console.log(`Listening on port ${PORT}`);
+});
